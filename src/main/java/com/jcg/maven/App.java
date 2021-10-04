d06eee4a617e6fcd10974da7c780a7ca12428611
Ramky <rama@example.com>
2021-10-04 11:31:25 +0530

    adding code has to be done

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! and update code has to be added" );
+        System.out.println( "Hello World! and update code has to new code" );
     }
 }
