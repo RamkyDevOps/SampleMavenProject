4181d3947941f92ecc8405cb2dbcf8cfd617c740
Ashokdevops <ashok123@gmail>
2021-10-06 00:27:42 -0700

    Hi new code

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! and update code has to new code" );
+        System.out.println( "Hello World hi ! and update code has to new code" );
     }
 }
